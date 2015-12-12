6c1978396623b493dfbad762f6af8f24602c90c6
Benjamin Monis <benjaminmonis@Benjamins-MacBook-Pro.local>
2015-12-12 01:17:02 -0500

    Update gulpfile for build

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var del = require('del');
 var mainBowerFiles = require('main-bower-files');
+
+gulp.task('clean:lib', function() {
+	return del([
+			'lib'
+		]);
+});
 
 gulp.task('bower', function() {
   return bower()
-    .pipe(gulp.dest('lib/'))
+    .pipe(gulp.dest('bower_components/'))
 });
 
-gulp.task("bower-files", function(){
+gulp.task("bower-files", ['clean:lib', 'bower'], function(){
     return gulp.src(mainBowerFiles({debugging: true}), { base: './bower_components' })
     	.pipe(gulp.dest("./lib"));
 });
-
-gulp.task('install', ['bower']);
 
 gulp.task('default', function() {
 	
